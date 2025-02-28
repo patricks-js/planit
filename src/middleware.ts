1e5d013de6d6808fa95a8bff216ba2e166ce90df
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2025-02-28 00:34:10 -0300

    temporary fix add todo in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,11 @@ import { type NextRequest, NextResponse } from "next/server";
 
 const authRoutes = ["/sign-in", "/sign-up"];
 
+// ! fix: enhance protected/public routes and adjust redirects
+
 export async function middleware(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
   const pathname = request.nextUrl.pathname;
-
-  if (!sessionCookie) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
-  }
 
   if (sessionCookie && authRoutes.some((route) => route === pathname)) {
     return NextResponse.redirect(new URL("/inbox", request.url));
