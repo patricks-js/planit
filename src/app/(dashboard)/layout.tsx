f99a2a3f66cffec9c613cc6fceec1c3bdb306e11
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2025-03-02 14:25:10 -0300

    add metadata to dashboard app

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,13 @@
 import { Header } from "@/components/header";
 import Submenu from "@/components/submenu";
+import type { Metadata } from "next";
 import type { PropsWithChildren } from "react";
+
+export const metadata: Metadata = {
+  title: "Planit - App",
+  description:
+    "Planit is a task management app that helps you stay organized and productive.",
+};
 
 export default function DashboardLayout({
   children,
