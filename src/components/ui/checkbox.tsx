5c5b99b3fb2a8c460c963280e6e4246632dff95d
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2025-03-02 15:44:46 -0300

    animate checkbox with `motion` lib

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,10 +1,44 @@
 "use client";
 
-import type * as React from "react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-import { CheckIcon } from "lucide-react";
+import { motion } from "motion/react";
+import type * as React from "react";
 
 import { cn } from "@/lib/utils";
+
+function CheckIcon() {
+  return (
+    <svg
+      aria-hidden="true"
+      width="20"
+      height="20"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <motion.path
+        d="M5 7.5L7 9.5L7.40859 8.81902C8.13346 7.6109 9.00376 6.49624 10 5.5V5.5"
+        className="stroke-white dark:stroke-black"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        initial={{
+          pathLength: 0,
+        }}
+        animate={{
+          pathLength: 1,
+        }}
+        exit={{
+          pathLength: 0,
+        }}
+        transition={{
+          delay: 0.025,
+          duration: 0.35,
+        }}
+      />
+    </svg>
+  );
+}
 
 function Checkbox({
   className,
@@ -14,16 +48,23 @@ function Checkbox({
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer size-4 shrink-0 rounded-[4px] border border-input shadow-xs outline-none transition-shadow focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 data-[state=checked]:border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:aria-invalid:ring-destructive/40",
+        "peer size-5 shrink-0 appearance-none rounded-sm border border-input shadow-xs outline-none transition-shadow focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 data-[state=checked]:border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:aria-invalid:ring-destructive/40",
         className,
       )}
       {...props}
     >
       <CheckboxPrimitive.Indicator
         data-slot="checkbox-indicator"
-        className="flex items-center justify-center text-current transition-none"
+        className="flex items-center justify-center text-current"
       >
-        <CheckIcon className="size-3.5" />
+        <motion.div
+          className="h-[inherit] w-[inherit] rounded-sm bg-black dark:bg-white"
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 400, damping: 30 }}
+        >
+          <CheckIcon />
+        </motion.div>
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   );
