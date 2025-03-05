8d23d10275e4b20b63ac9114492b89612371898d
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2025-03-05 15:34:19 -0300

    minor changes in auth flow

--- src/components/profile-menu-logout.tsx
+++ src/components/profile-menu-logout.tsx
@@ -1,12 +1,25 @@
 "use client";
 
-import { signOutAction } from "@/app/actions/signout-action";
+import { authClient } from "@/lib/auth-client";
 import { LogOutIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { DropdownMenuItem } from "./ui/dropdown-menu";
 
 export function ProfileMenuLogout() {
+  const router = useRouter();
+
+  async function handleSignOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/login");
+        },
+      },
+    });
+  }
+
   return (
-    <DropdownMenuItem onClick={() => signOutAction()}>
+    <DropdownMenuItem onClick={() => handleSignOut()}>
       <LogOutIcon
         size={16}
         className="opacity-60"
